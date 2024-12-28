a580bdaa178faa48d240bb4edafd0fe15ea51889
ns3596 <nihariesri@gmail.com>
2024-12-28 03:17:29 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,6 +27,7 @@ table_name = st.secrets["bigquery"]["table_name"]
 reference_table_name = st.secrets["bigquery"]["reference_table_name"]
 citation_table_name = st.secrets["bigquery"]["citation_table_name"]
 cross_encoder  = st.secrets["bigquery"]["cross_encoder"]
+st.write("testingt!")
 bq_storage_client = bigquery_storage.BigQueryReadClient()
 
 st.write("Reached further of script!")
