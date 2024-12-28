4a4afe858d9c78d04941a5a7a0f32e7f36fd2fb6
ns3596 <nihariesri@gmail.com>
2024-12-28 01:32:28 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,8 +15,8 @@ from sklearn.preprocessing import MinMaxScaler
 
 st.set_page_config(layout="wide")
 
-service_account_json_str = st.secrets["gcp_service_account"]
-service_account_info = json.loads(service_account_json_str)
+service_account_json_str = st.secrets["gcp_service_account"]["json"]
+service_account_info = json.loads(service_account_json_str) 
 
 from google.oauth2.service_account import Credentials
 credentials = Credentials.from_service_account_info(service_account_info)
